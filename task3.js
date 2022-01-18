e4dc776bbca896baf8c10f21e037b9ed24d477f3
rintosaputro <saputrorinto@gmail.com>
2022-01-18 15:31:03 +0700

    melanjutkan pengerjaa tugas 3

--- task3.js
+++ task3.js
@@ -1,10 +1,17 @@
 const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
     if(nilaiAwal < nilaiAkhir) {
         if(dataArray.length > 5) {
-            
+            const filterAwal = dataArray.filter(data => data >= nilaiAwal)
+            const filterAkhir = filterAwal.filter(data => data <= nilaiAkhir)
+            filterAkhir.sort((a, b) => a-b);
+            console.log(filterAkhir)
+        } else {
+            console.log("Jumlah angka dalam dataArray harus lebih dari 5")
         }
 
     } else {
         console.log("Nilai akhir harus lebih dari nilai awal")
     }
 }
+
+seleksiNilai(5,20,[2,25,4,14,17,30,8])
