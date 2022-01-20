96d0e3e9af68874ed66f5024728a890fbd5cfb46
rintosaputro <saputrorinto@gmail.com>
2022-01-20 12:01:10 +0700

    menambah command penjelasan program

--- task2.js
+++ task2.js
@@ -4,19 +4,21 @@ const name = [
     "Carol", "Caroline", "Carolyn",
     "Deirdre", "Diana", "Elizabeth",
     "Ella", "Faith", "Olivia", "Penelope"
-]
+] // inisialisasi array name
 
-const searchName = (key, batas, cb) => {
-    const pola = new RegExp(key)
-    const cari = name.filter(data => {
-        return pola.test(data.toLowerCase())
+const searchName = (key, batas, cb) => { // inisialisasi fungsi dengan parameter key, batas, dan cb
+    const pola = new RegExp(key) // menentukan pola dengan regular expression dimana nilai regex diisi dengan parameter key
+    const cari = name.filter(data => { //inisialisasi cari yang akan memfilter array name dengan callback function berisi parameter data
+        return pola.test(data.toLowerCase()) // mengembalikan nilai dengan method regex .test yang akan mengembalikan boolean dengan mencocokan niali dari pola dengan data(niali dari tiap array name) yang diubah menjadi huruf kecil semua
     })
-    cb(cari, batas)
+    cb(cari, batas) // memanggil callback function cb dengan parameter diisi cari dan batas
 }
 
-const callback = (arr, batas) => {
-    const filt = arr.filter((_item, index) => index < batas) 
-    console.log(filt)
+const callback = (arr, batas) => { // inisialisasi fungsi callback dengan parameter arr dan batas
+    const filt = arr.filter((_item, index) => index < batas) // memfilter array dari parameter arr 
+    // filter diisi degan dua parameter _item (menggunakan _ karenan parameter tidak digunakan) dan index yang akan mereturn index yang kurang dari batas
+    console.log(filt) // mencetak hasil filter
 }
 
-searchName("an", 3, callback)
+searchName("an", 3, callback) // memanggil fungsi searchName
+// output => [ 'Alexandra', 'Amanda', 'Angela' ]
--- task3.js
+++ task3.js
@@ -1,17 +1,17 @@
-const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
-    if(nilaiAwal < nilaiAkhir) {
-        if(dataArray.length > 5) {
-            const filterAwal = dataArray.filter(data => data >= nilaiAwal)
-            const filterAkhir = filterAwal.filter(data => data <= nilaiAkhir)
-            filterAkhir.sort((a, b) => a-b);
-            console.log(filterAkhir)
-        } else {
-            console.log("Jumlah angka dalam dataArray harus lebih dari 5")
+const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => { // inisialisasi fungsi seleksiNilai dengan parameter nilaiAwal (number), nilaiAkhir(number), dataArray(array)
+    if(nilaiAwal < nilaiAkhir) { // validasi apakah nialaiAwal kurang dari nilaiAkhir
+        if(dataArray.length > 5) { // validasi apakah panjang dari dataArray lebih dari 5
+            const filterAwal = dataArray.filter(data => data >= nilaiAwal) // memfilter dataArray yang mengembalikan nilainya lebih dari sama dengan nilaiAwal
+            const filterAkhir = filterAwal.filter(data => data <= nilaiAkhir) // memfilter filterAwal yang mengembalikan nilainya kurang dari sama dengan nilaiAkhir
+            filterAkhir.sort((a, b) => a-b); // mengurutkan nilai filterAkhir dengan sort dengan callback function parameter a dan b di mana jika a -b < 0 maka urutan a b atau jika a-b >0 maka b a atau jika a-b == 0 maka urutan tetap
+            console.log(filterAkhir) // mencetak filterAkhir yang sudah diurutkan nilainya tadi
+        } else { // inisialisasi pengkondisian false dataArray>5
+            console.log("Jumlah angka dalam dataArray harus lebih dari 5") // mencetak pesan yang false
         }
 
-    } else {
-        console.log("Nilai akhir harus lebih besar dari nilai awal")
+    } else { // inisialisasi pengkondisian false nilaiAwal<nilaiAkhir
+        console.log("Nilai akhir harus lebih besar dari nilai awal") // mencetak pesan untuk pengkondisian false
     }
 }
 
-seleksiNilai(5,20,[2,25,4,14,17,30,8])
+seleksiNilai(5,20,[2,25,4,14,17,30,8]) // memanggil fungsi seleksiNilai dengan parameter pertama dan kedua adalah number dan parameter ketiga array
