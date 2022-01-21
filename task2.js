98d80281c4a4617558f2b7d35eb5953dd8e0f8c2
rintosaputro <saputrorinto@gmail.com>
2022-01-21 07:42:53 +0700

    penambahan incasesensitive pada regex

--- task2.js
+++ task2.js
@@ -7,10 +7,9 @@ const name = [
 ] // inisialisasi array name
 
 const searchName = (key, batas, cb) => { // inisialisasi fungsi dengan parameter key, batas, dan cb
-    const lowerKey = key.toLowerCase() // mengubah nilai dari parameter key menjadi huruf kecil semua
-    const pola = new RegExp(lowerKey) // menentukan pola dengan regular expression dimana nilai regex diisi dengan lowerKey
+    const pola = new RegExp(key, 'i') // menentukan pola dengan regular expression dimana nilai regex diisi dengan key dan parameter kedua 'i' yang berarti incasesensitive
     const cari = name.filter(data => { //inisialisasi cari yang akan memfilter array name dengan callback function berisi parameter data
-        return pola.test(data.toLowerCase()) // mengembalikan nilai dengan method regex .test yang akan mengembalikan boolean dengan mencocokan niali dari pola dengan data(niali dari tiap array name) yang diubah menjadi huruf kecil semua
+        return pola.test(data) // mengembalikan nilai dengan method regex .test yang akan mengembalikan boolean dengan mencocokan niali dari pola dengan data(niali dari tiap array name)
     })
     cb(cari, batas) // memanggil callback function cb dengan parameter diisi cari dan batas
 }
