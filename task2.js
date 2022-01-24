c0b06b106ad5056984a2c6efcff1327d0b5a7543
rintosaputro <saputrorinto@gmail.com>
2022-01-24 20:18:04 +0700

    perbaikan penulisan program

--- task2.js
+++ task2.js
@@ -1,23 +1,22 @@
-const name = [
-    "Abigail", "Alexandra", "Alison",
-    "Amanda", "Angela", "Bella",
-    "Carol", "Caroline", "Carolyn",
-    "Deirdre", "Diana", "Elizabeth",
-    "Ella", "Faith", "Olivia", "Penelope"
-] // inisialisasi array name
-
 const searchName = (key, batas, cb) => { // inisialisasi fungsi dengan parameter key, batas, dan cb
+    const name = [
+        "Abigail", "Alexandra", "Alison",
+        "Amanda", "Angela", "Bella",
+        "Carol", "Caroline", "Carolyn",
+        "Deirdre", "Diana", "Elizabeth",
+        "Ella", "Faith", "Olivia", "Penelope"
+    ] // inisialisasi array name
+
     const pola = new RegExp(key, 'i') // menentukan pola dengan regular expression dimana nilai regex diisi dengan key dan parameter kedua 'i' yang berarti incasesensitive
     const cari = name.filter(data => { //inisialisasi cari yang akan memfilter array name dengan callback function berisi parameter data
         return pola.test(data) // mengembalikan nilai dengan method regex .test yang akan mengembalikan boolean dengan mencocokan niali dari pola dengan data(niali dari tiap array name)
     })
-    cb(cari, batas) // memanggil callback function cb dengan parameter diisi cari dan batas
+    const hasil = cari.filter((_item, index) => index < batas) // memfilter array dari variable cari, filter diisi degan dua parameter _item (menggunakan _ karenan parameter tidak digunakan) dan index yang akan mereturn index yang kurang dari batas
+    cb(hasil) // memanggil callback function cb dengan parameter diisi hasil
 }
 
-const callback = (arr, batas) => { // inisialisasi fungsi callback dengan parameter arr dan batas
-    const filt = arr.filter((_item, index) => index < batas) // memfilter array dari parameter arr 
-    // filter diisi degan dua parameter _item (menggunakan _ karenan parameter tidak digunakan) dan index yang akan mereturn index yang kurang dari batas
-    console.log(filt) // mencetak hasil filter
+const callback = (res) => { // inisialisasi fungsi callback dengan parameter res
+    console.log(res) // mencetak nilai dari parameter res
 }
 
 searchName("an", 3, callback) // memanggil fungsi searchName
--- task3.js
+++ task3.js
@@ -1,15 +1,19 @@
 const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => { // inisialisasi fungsi seleksiNilai dengan parameter nilaiAwal (number), nilaiAkhir(number), dataArray(array)
-    if(nilaiAwal < nilaiAkhir) { // validasi apakah nialaiAwal kurang dari nilaiAkhir
-        if(dataArray.length > 5) { // validasi apakah panjang dari dataArray lebih dari 5
-            const filterData = dataArray.filter(data => data >= nilaiAwal && data <= nilaiAkhir) // memfilter dataArray yang mengembalikan data >= nilaiAwal && data <= nilaiAkhir
-            filterData.sort((a, b) => a-b); // mengurutkan nilai filterData dengan sort dengan callback function parameter a dan b di mana jika a -b < 0 maka urutan a b atau jika a-b >0 maka b a atau jika a-b == 0 maka urutan tetap
-            console.log(filterData) // mencetak filterData yang sudah diurutkan nilainya tadi
-        } else { // inisialisasi pengkondisian false dataArray>5
-            console.log("Jumlah angka dalam dataArray harus lebih dari 5") // mencetak pesan yang false
-        }
+    if(typeof(nilaiAwal) == 'number' && typeof(nilaiAkhir) == 'number' && Array.isArray(dataArray)) { // validasi apakah nilai parameter nilaiAwal dan nilaiAkhir adalah number dan dataArray adalah array
+        if(nilaiAwal < nilaiAkhir) { // validasi apakah nialaiAwal kurang dari nilaiAkhir
+            if(dataArray.length > 5) { // validasi apakah panjang dari dataArray lebih dari 5
+                const filterData = dataArray.filter(data => data >= nilaiAwal && data <= nilaiAkhir) // memfilter dataArray yang mengembalikan data >= nilaiAwal && data <= nilaiAkhir
+                filterData.sort((a, b) => a-b); // mengurutkan nilai filterData dengan sort dengan callback function parameter a dan b di mana jika a -b < 0 maka urutan a b atau jika a-b >0 maka b a atau jika a-b == 0 maka urutan tetap
+                console.log(filterData) // mencetak filterData yang sudah diurutkan nilainya tadi
+            } else { // inisialisasi pengkondisian false dataArray>5
+                console.log("Jumlah angka dalam dataArray harus lebih dari 5") // mencetak pesan yang false
+            }
 
-    } else { // inisialisasi pengkondisian false nilaiAwal<nilaiAkhir
-        console.log("Nilai akhir harus lebih besar dari nilai awal") // mencetak pesan untuk pengkondisian false
+        } else { // inisialisasi pengkondisian false nilaiAwal<nilaiAkhir
+            console.log("Nilai akhir harus lebih besar dari nilai awal") // mencetak pesan untuk pengkondisian false
+        }
+    } else { // pengkondisian jika false dari validasi type data parameter seleksiNilai
+        console.log('Inputan data tidak sesuai') // mencetak pesan untuk pengkondisian false
     }
 }
 
