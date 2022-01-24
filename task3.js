06f006fbb8cea25a9033beaaaca53295bbc744a8
rintosaputro <saputrorinto@gmail.com>
2022-01-24 07:40:02 +0700

    penyerdahanaan code filter

--- task3.js
+++ task3.js
@@ -1,10 +1,9 @@
 const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => { // inisialisasi fungsi seleksiNilai dengan parameter nilaiAwal (number), nilaiAkhir(number), dataArray(array)
     if(nilaiAwal < nilaiAkhir) { // validasi apakah nialaiAwal kurang dari nilaiAkhir
         if(dataArray.length > 5) { // validasi apakah panjang dari dataArray lebih dari 5
-            const filterAwal = dataArray.filter(data => data >= nilaiAwal) // memfilter dataArray yang mengembalikan nilainya lebih dari sama dengan nilaiAwal
-            const filterAkhir = filterAwal.filter(data => data <= nilaiAkhir) // memfilter filterAwal yang mengembalikan nilainya kurang dari sama dengan nilaiAkhir
-            filterAkhir.sort((a, b) => a-b); // mengurutkan nilai filterAkhir dengan sort dengan callback function parameter a dan b di mana jika a -b < 0 maka urutan a b atau jika a-b >0 maka b a atau jika a-b == 0 maka urutan tetap
-            console.log(filterAkhir) // mencetak filterAkhir yang sudah diurutkan nilainya tadi
+            const filterData = dataArray.filter(data => data >= nilaiAwal && data <= nilaiAkhir) // memfilter dataArray yang mengembalikan data >= nilaiAwal && data <= nilaiAkhir
+            filterData.sort((a, b) => a-b); // mengurutkan nilai filterData dengan sort dengan callback function parameter a dan b di mana jika a -b < 0 maka urutan a b atau jika a-b >0 maka b a atau jika a-b == 0 maka urutan tetap
+            console.log(filterData) // mencetak filterData yang sudah diurutkan nilainya tadi
         } else { // inisialisasi pengkondisian false dataArray>5
             console.log("Jumlah angka dalam dataArray harus lebih dari 5") // mencetak pesan yang false
         }
